b64976f0e48e7488221a17a25410b13735784678
Christian Tondo <christiantondo1@gmail.com>
2026-04-18 21:59:54 +0200

    question 3

--- main.js
+++ main.js
@@ -37,3 +37,27 @@
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // 3 in totale, anche se l'array ingredients all'interno di secondBurger fa riferimento all'array originale.
+
+////////////////////////////////////////////////////////////
+// Code Question 3
+
+// const hamburger = {
+//     name: "Cheese Burger",
+//     weight: 250,
+//     maker: {
+//         name: "Anonymous Chef",
+//         restaurant: {
+//             name: "Hyur's Burgers",
+//             address: "Main Street, 123",
+//             isOpen: true,
+//         },
+//         age: 29
+//     }
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+////////////////////////////////////////////////////////////
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// Totale 9, 6 nuovi.
