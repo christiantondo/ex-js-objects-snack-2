b5f11e04f1596a8139735dc1e929dacc31dc5ca4
Christian Tondo <christiantondo1@gmail.com>
2026-04-20 13:46:15 +0200

    question 5

--- main.js
+++ main.js
@@ -65,29 +65,29 @@
 ////////////////////////////////////////////////////////////
 // Code Question 4
 
-const chef = {
-    name: "Chef Hyur",
-    age: 29,
-    makeBurger: (num = 1) => {
-        console.log(`Ecco ${num} hamburger per te!`);
-    },
-}
+// const chef = {
+// 	name: "Chef Hyur",
+// 	age: 29,
+// 	makeBurger: (num = 1) => {
+// 		console.log(`Ecco ${num} hamburger per te!`);
+// 	},
+// }
 
-const restaurant = {
-    name: "Hyur's Burgers",
-    address: {
-        street: 'Main Street',
-        number: 123,
-    },
-    openingDate: new Date(2025, 3, 11),
-    isOpen: false,
-};
+// const restaurant = {
+// 	name: "Hyur's Burgers",
+// 	address: {
+// 		street: 'Main Street',
+// 		number: 123,
+// 	},
+// 	openingDate: new Date(2025, 3, 11),
+// 	isOpen: false,
+// };
 
-const chefCopy = { ...chef };
-const restaurantCopy = structuredClone(restaurant);
+// const chefCopy = { ...chef };
+// const restaurantCopy = structuredClone(restaurant);
 
-console.log(chefCopy);
-console.log(restaurantCopy);
+// console.log(chefCopy);
+// console.log(restaurantCopy);
 ////////////////////////////////////////////////////////////
 
 // Qual è il metodo migliore per clonare l’oggetto chef, e perché?
@@ -95,3 +95,39 @@ console.log(restaurantCopy);
 
 // Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
 // Il metodo migliore per copiare l'oggetto restaurant è lo structuredClone in quanto può effettuare una deep copy e può copiare oggetti annidati e complessi.
+
+////////////////////////////////////////////////////////////
+// Code Question 5 (Bonus)
+
+const hamburger = {
+	name: "Cheese Burger",
+	weight: 250,
+	maker: {
+		name: "Anonymous Chef",
+		restaurant: {
+			name: "Hyur's Burgers",
+			address: "Main Street, 123",
+			isOpen: true,
+		},
+		age: 29
+	}
+};
+
+const newRestaurant = { ...hamburger.maker.restaurant };
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurger = { ...hamburger };
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+
+console.log(hamburger.maker.name); // ?
+console.log(secondBurger.maker.name); // ?
+console.log(hamburger.maker.restaurant.name); // ?
+console.log(secondBurger.maker.restaurant.name); // ?
+////////////////////////////////////////////////////////////
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Verranno stampati due volte Chef Hyur e Hyur's II.
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// In memoria in totale ci sono 5 oggetti.
